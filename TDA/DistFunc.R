17821db33fa975eb886745f64947730381609660
Wang chi-chien <91866898+kennywang112@users.noreply.github.com>
2024-03-06 12:08:37 +0800

    Rips Diagrams

--- TDA/DistFunc.R
+++ TDA/DistFunc.R
@@ -9,7 +9,7 @@ by <- 0.065
 Xseq <- seq(Xlim[1], Xlim[2], by = by)
 Yseq <- seq(Ylim[1], Ylim[2], by = by)
 Grid <- expand.grid(Xseq, Yseq)
-par(mfrow = c(1, 2))
+par(mfrow = c(1, 1))
 plot(X, col = 2, pch = 19, cex = 1.5)
 plot(Grid, col = 2, pch = 19, cex = 1.5)
 
@@ -55,28 +55,44 @@ DiagGrid <- gridDiag(
   printProgress = FALSE)
 plot(DiagGrid[["diagram"]], band = 2 * band[["width"]],
     main = "KDE Diagram")
+# Rips Diagrams
+par(mfrow = c(1, 1))
+plot(DiagGrid[["diagram"]], rotated = TRUE, band = band[["width"]],
+       main = "Rotated Diagram")
+plot(DiagGrid[["diagram"]], barcode = TRUE, main = "Barcode")
+
+Circle1 <- circleUnif(60)
+Circle2 <- circleUnif(60, r = 2) + 3
+Circles <- rbind(Circle1, Circle2)
+maxscale <- 1
+maxdimension <- 1
+DiagRips <- ripsDiag(X = Circles, maxdimension, maxscale,
+                     library = c("GUDHI", "Dionysus"), location = TRUE, printProgress = FALSE)
+
+plot(Circles, col = 2, pch = 19, cex = 1.5)
+lines(DiagRips$deathLocation)
+
+plot(DiagRips[["diagram"]], main = "Rips Diagram")
+
+# Alpha Complex Persistence Diagram
+X <- circleUnif(n = 30)
+DiagAlphaCmplx <- alphaComplexDiag(
+  X = X, library = c("GUDHI", "Dionysus"), location = TRUE,
+  printProgress = TRUE)
+par(mfrow = c(1, 2))
+plot(DiagAlphaCmplx[["diagram"]], main = "Alpha complex persistence diagram")
+one <- which(DiagAlphaCmplx[["diagram"]][, 1] == 1)
+one <- one[which.max(
+  DiagAlphaCmplx[["diagram"]][one, 3] - DiagAlphaCmplx[["diagram"]][one, 2])]
+plot(X, col = 1, main = "Representative loop")
+for (i in seq(along = one)) {
+  for (j in seq_len(dim(DiagAlphaCmplx[["cycleLocation"]][[one[i]]])[1])) {
+    lines(DiagAlphaCmplx[["cycleLocation"]][[one[i]]][j, , ], pch = 19,
+          cex = 1, col = i + 1)
+  }
+}
+par(mfrow = c(1, 1))
 
 
 
-data(iris)
-iris%>%ggplot()+geom_point(aes(Sepal.Length, Petal.Length, color = Species))
-Xiris <- iris%>%select(Sepal.Length, Petal.Length)
-Xiris$Sepal.Length%>%summary()
-Xiris$Petal.Length%>%summary()
-Xlim <- c(1, 8)
-Ylim <- c(1, 8)
-by <- 0.05
-Xseq <- seq(Xlim[1], Xlim[2], by = by)
-Yseq <- seq(Ylim[1], Ylim[2], by = by)
-Grid <- expand.grid(Xseq, Yseq)
-# Grid%>%ggplot()+geom_point(aes(Var1, Var2))
-par(mfrow = c(1, 2), bg = "gray")
-KDE <- kde(X = Xiris, Grid = Grid, h = 0.3)
-# iris_dist <- distFct(X = Xiris, Grid = Grid)
-persp(Xseq, Yseq,
-      matrix(KDE, ncol = length(Yseq), nrow = length(Xseq)), xlab = "",
-      ylab = "", zlab = "", theta = 30, phi = 20, ltheta = 50,
-      border = NA, main = "distance", d = 0.5, scale = FALSE, box = TRUE, col = "brown",
-      expand = 3, shade = 0.9)
-plot(Xiris, col = iris$Species, pch = 19, cex = 1.5)
 
